c3ce78c30618f9eb7565b9e81dacd9f9b2270c0a
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-04 22:52:05 -0300

    chore: add missing styled components config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,49 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="pt-br">
-                <Head>
-                    <meta charSet="utf-8" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
-                        rel="stylesheet"
-                    />
-                </Head>
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-                <Main />
-                <NextScript />
-            </Html>
-        );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <Main />
+        <NextScript />
+      </Html>
+    );
+  }
 }
