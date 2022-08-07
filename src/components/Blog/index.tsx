7b1c48c9e206ec443423abb521da7337e95681ed
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-07 17:28:10 -0300

    refactor: :recycle: blog component

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,44 +1,33 @@
-/* eslint-disable max-len */
-import styles from './styles.module.scss';
+import { Article, Container, Content } from './styles';
 
 export function Blog() {
-    return (
-        <div className={styles.blogarea}>
-            <div className={styles.blogAreaContent}>
-                <h1>tech talk</h1>
-                <div className={styles.articlesArea}>
-                    <div className={styles.article}>
-                        <div className={styles.contentArticle}>
-                            <h2>lorem Ipsum</h2>
-                            <p>
-                                Lorem Ipsum is simply dummy text of the printing
-                                and typesetting industry. Lorem Ipsum has been
-                                the industrys standard dummy text ever since the
-                                1500s, when an unknown printer took a galley of
-                                type and scrambled it to make a type specimen
-                                book. It has survived not only five centuries,
-                                but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
-                            </p>
-                        </div>
-                    </div>
-                    <div className={styles.article}>
-                        <div className={styles.contentArticle}>
-                            <h2>lorem Ipsum</h2>
-                            <p>
-                                Lorem Ipsum is simply dummy text of the printing
-                                and typesetting industry. Lorem Ipsum has been
-                                the industrys standard dummy text ever since the
-                                1500s, when an unknown printer took a galley of
-                                type and scrambled it to make a type specimen
-                                book. It has survived not only five centuries,
-                                but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <Container>
+      <h1>tech talk</h1>
+      <Content>
+        <Article>
+          <h2>lorem Ipsum</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </Article>
+        <Article>
+          <h2>lorem Ipsum</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </Article>
+      </Content>
+    </Container>
+  );
 }
---
+++ src/components/Blog/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1170px;
+  padding: 100px 0 0 0;
+
+  h1 {
+    color: #fca311;
+    font-size: 3.25rem;
+    width: 100%;
+    max-width: 800px;
+  }
+`;
+
+export const Content = styled.section`
+  width: 100%;
+  max-width: 800px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Article = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  max-width: 300px;
+
+  h2 {
+    font-weight: 700;
+    font-size: 2rem;
+    color: #14213d;
+
+    transition: 0.2s;
+    &:hover {
+      color: #eb9710;
+    }
+  }
+
+  p {
+    padding-top: 1.25rem;
+    font-size: 0.875rem;
+  }
+`;
