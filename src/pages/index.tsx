7891431d58ea972ce45b428e57f37a09cf877d0f
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-07 16:50:11 -0300

    feat: :sparkles: apply half  card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,41 @@
 import { Blog } from '../components/Blog';
-import { CardLeft } from '../components/CardLeft';
-import { CardRight } from '../components/CardRight';
 import { HalfCard } from '../components/HalfCard';
 import { RepositoriesCards } from '../components/RepositoriesCards';
 import { TopText } from '../components/TopText';
 
 export default function Home() {
-    return (
-        <div className="home">
-            <TopText />
-            <CardRight />
-            <CardLeft />
-            <HalfCard />
-            <RepositoriesCards />
-            <Blog />
-        </div>
-    );
+  return (
+    <div className="home">
+      <TopText />
+
+      <HalfCard
+        titleSide="right"
+        title="saiba mais sobre mim"
+        link={{
+          title: 'about',
+          href: '/about',
+        }}
+      />
+
+      <HalfCard
+        titleSide="left"
+        title="minha formação profissional"
+        link={{
+          title: 'tech',
+          href: '/tech',
+        }}
+      />
+
+      <HalfCard
+        titleSide="right"
+        title="desafios ativos"
+        link={{
+          title: 'github',
+          href: 'https://github.com/duquedotdev',
+        }}
+      />
+      <RepositoriesCards />
+      <Blog />
+    </div>
+  );
 }
