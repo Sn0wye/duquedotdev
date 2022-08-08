09cf370b8fe16066158b4a2975758661436efa4d
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-08 18:46:35 -0300

    feat: :sparkles: implement tech cards at tech page

--- src/pages/tech/index.tsx
+++ src/pages/tech/index.tsx
@@ -1,18 +1,39 @@
-import { RepositoriesCards } from '../../components/RepositoriesCards';
-import { TechCardsTextMidle } from '../../components/TechCardsTextBoth';
-import { TechCardsTextLeft } from '../../components/TechCardsTextLeft';
-import { TechCardsTextRight } from '../../components/TechCardsTextRight';
+import { HalfCard } from '../../components/HalfCard';
+import { TechCardsRow } from '../../components/TechCardsRow';
 import { Title } from '../../components/Title';
 
 export default function Tech() {
   return (
     <div className="tech">
       <Title />
-      <TechCardsTextLeft />
-      <RepositoriesCards />
-      <TechCardsTextRight />
-      {/* <HalfCard showTitle={false} /> */}
-      <TechCardsTextMidle />
+      <TechCardsRow
+        textCard={{
+          title: 'ai & ml',
+          content:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500.',
+          position: 'left',
+        }}
+      />
+
+      <TechCardsRow />
+      <TechCardsRow
+        textCard={{
+          title: 'aviation',
+          content:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500.',
+          position: 'right',
+        }}
+      />
+
+      <HalfCard showTitle={false} title="" titleSide="right" />
+      <TechCardsRow
+        textCard={{
+          title: 'software qa',
+          content:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500.',
+          position: 'middle',
+        }}
+      />
       {/* <Nutshell /> */}
     </div>
   );
