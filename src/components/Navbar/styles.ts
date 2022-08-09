c7c3b48ed466f1aba0f99d7c18f84e0518a4db77
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-08-09 19:39:23 -0300

    style: :iphone: add navbar responsiveness

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,36 +1,46 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-    background-color: transparent;
-    width: 100%;
-    max-width: 1170px;
-    display: flex;
-    position: fixed;
-    justify-content: space-between;
-    align-items: center;
+  background: transparent;
+
+  width: 100%;
+  max-width: calc(1170px + 2rem);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.25rem 2rem;
+  margin: 0 auto;
+  position: sticky;
+  top: 0;
+
+  @media (max-width: 1202px) {
+    // 1170px + 2rem(padding)
+    position: relative;
+  }
 `;
 
 export const Logo = styled.h1`
-    font-weight: 500;
-    font-size: 1.2rem;
-    cursor: pointer;
+  font-weight: 500;
+  font-size: 1.2rem;
+  cursor: pointer;
 `;
 
-export const Navbar = styled.nav`
-    ul {
-        display: flex;
-        align-items: center;
-        text-decoration: none;
-        list-style: none;
-    }
+export const StyledNavbar = styled.nav`
+  ul {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    list-style: none;
+  }
 `;
 
 export const Anchor = styled.li`
-    margin-left: 1.5rem;
-    transition: color 0.2s;
-    cursor: pointer;
+  margin-left: 1.5rem;
+  transition: color 0.2s;
+  cursor: pointer;
 
-    &:hover {
-        color: #fca311;
-    }
+  &:hover {
+    color: #fca311;
+  }
 `;
